1a50ad5a5bb0fe0213d07954288f83ddd0b16c32
VinHylme <Lizodesignergfx@gmail.com>
2022-05-16 10:34:20 +0100

    tabs 3

--- components/organisms/pricesSection.tsx
+++ components/organisms/pricesSection.tsx
@@ -33,7 +33,7 @@ export const PricesSection: React.FC<IPricesSectionProps> = ({tokenId = 'vidya',
   return (
     <div className='w-full h-full flex flex-col m-0 p-4 justify-center items-center flex-wrap'>
       <div className='w-full flex sm:gap-x-2 gap-x-1 gap-y-4 flex-wrap'>
-        <PriceCard length={isMobileView ? 'sm' : 'md'} label='PRICE' price={data?.currentPrice[currencySelected]?.value?.toFixed(7) || 0} perctChange={data?.currentPrice[currencySelected].changePercentage24h || 0} increase="auto"></PriceCard>
+        <PriceCard length={isMobileView ? 'sm' : 'md'} label='PRICE' price={data?.currentPrice[currencySelected]?.value?.toFixed(6) || 0} perctChange={data?.currentPrice[currencySelected].changePercentage24h || 0} increase="auto"></PriceCard>
         <PriceCard length={isMobileView ? 'sm' : 'md'} label='MARKET CAP' price={formatPrice(data?.marketCap[currencySelected]?.value)} perctChange={data?.marketCap[currencySelected]?.changePercentage24h || 0} increase="auto"></PriceCard>
         <PriceCard length={isMobileView ? 'sm' : 'md'} label='24HR VOL' price={formatPrice(data?.volume?.[currencySelected].value)} perctChange={data?.volume[currencySelected]?.changePercentage24h || 0} increase="auto"></PriceCard>
         <PriceCardWithCustomFooter length={isMobileView ? 'sm' : 'md'} label='TOTAL SUPPLY' price={formatPrice(data?.totalSupply)} footer={`${data?.circulatingSupply?.toFixed(2) || 0} circulating`}/>
