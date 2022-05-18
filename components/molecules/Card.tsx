9b4e7fd0c3d73ec368d3dde6aa187487e3253c52
VinHylme <Lizodesignergfx@gmail.com>
2022-05-18 08:59:18 +0100

    basic card gap

--- components/molecules/Card.tsx
+++ components/molecules/Card.tsx
@@ -40,7 +40,7 @@ export const Card: React.FC<ICardProps> = ({bordered=true, sameType=false, round
       </div>
     </VItemContainer>
     <VItemContainer center={center} showBorderTop={false} roundedTop={false} showBorder={bordered} widthSize={wide ? 'vxl' : 'vlg'} heightSize={ sameType ? 'vhlf' : footer ? 'vsm' : 'vxs'}>
-      <div className={classNames('w-full flex flex-col justify-center items-center', avatar ? 'mt-14' : 'mt-8','ml-2', footer ? 'space-y-4' : 'space-y-6', bordered ? '' : 'px-4 mt-0')}>
+      <div className={classNames('w-full flex flex-col justify-center items-center', avatar ? 'mt-vlrg' : 'mt-vmd','ml-2', footer ? 'gap-y-vmd' : 'gap-y-vmd', bordered ? '' : 'px-4 mt-0')}>
         <VTitle type='h4'>{title}</VTitle>
         <VText className={classNames(sameType ? 'min-h-[85px] max-h-[85px]' :'min-h-[52px] max-h-[52px] ', 'overflow-y-scroll scrollbar-track-rounded-full scrollbar-thin dark:scrollbar-thumb-light-300 scrollbar-thumb-dark-200')} size='md' weight='normal'>{subtitle}</VText>
         {footer && typeof(footer) === 'string' ? <VText size='sm'>{footer}</VText> : footer}
