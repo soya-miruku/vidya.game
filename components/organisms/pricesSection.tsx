7b3ab054b83ae8e567c73749058a43b11dd642e2
VinHylme <Lizodesignergfx@gmail.com>
2022-05-19 08:05:27 +0100

    price section provider clickable

--- components/organisms/pricesSection.tsx
+++ components/organisms/pricesSection.tsx
@@ -47,8 +47,8 @@ export const PricesSection: React.FC<IPricesSectionProps> = ({tokenId = 'vidya',
       </div>
       <div className='p-vlrg flex justify-between items-center w-full h-full'>
         <VTabs items={[{label: 'USD', value: 'usd'},{label: 'ETH', value: 'eth'}]} onChange={(val) => setCurrencySelected(val)}/>
-        <div className='flex space-x-4 space-x-reverse items-center'>
-          <VText className='px-2' size="md">Prices provided by <strong>Coingecko</strong></VText>
+        <div className='flex space-x-4 space-x-reverse items-center hover:cursor-pointer' onClick={() => window.open('https://www.coingecko.com/en/coins/vidya', '_blank')}>
+          <VText className='px-2' size="sm">Prices provided by <strong>Coingecko</strong></VText>
           <VImage width={25} height={25} src='https://static.coingecko.com/s/thumbnail-007177f3eca19695592f0b8b0eabbdae282b54154e1be912285c9034ea6cbaf2.png'/>
         </div>
       </div>
