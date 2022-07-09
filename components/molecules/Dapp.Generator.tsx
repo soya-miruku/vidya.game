08afd9a1a945eb1ede90b76e633146250ec57aa6
VinHylme <Lizodesignergfx@gmail.com>
2022-07-09 11:12:21 +0100

    generator update description position change

--- components/molecules/Dapp.Generator.tsx
+++ components/molecules/Dapp.Generator.tsx
@@ -11,13 +11,6 @@ export const GeneratorDapp = ({}) => {
   return (
     <>
       <div className="flex flex-col justify-between items-center py-vsm h-full">
-        <div className="w-full flex justify-start px-vlrg">
-          <div className="flex w-[350px] justify-center">
-              <p>
-                The VIDYA Staking platform rewards the best performing stakers with a portion of the total VIDYA supply. The rewards are calculated based on the total amount of VIDYA staked in the network.
-              </p>
-          </div>
-        </div>
         <div className="px-vsm">
           <VItemContainer widthSize='v2xl' heightSize='vhxl' dropShadow className='w-full h-full bg-gradient-to-br p-[1px] from-aimbotsRed-100/95 to-accent-dark-200/95 backdrop-blur-sm'>
             <div className='flex flex-col sm:p-vxl p-vlrg gap-y-vlrg h-full'>
--- components/organisms/ProgramModalEntry.tsx
+++ components/organisms/ProgramModalEntry.tsx
@@ -10,16 +10,21 @@ export interface IProgramModalEntryProps {
 export interface IProgramWrapperProps {
   onClose: () => void;
   title?: string;
+  description?: string;
   children: React.ReactNode;
 }
 
-export const ProgramModalWrapper = ({title, children, onClose}: IProgramWrapperProps) => {
+export const ProgramModalWrapper = ({title, description, children, onClose}: IProgramWrapperProps) => {
   return (
     <Modal onClose={onClose} className="h-full flex flex-col">
       <div className="w-full h-full flex flex-col dark:bg-dark-300/60 backdrop-blur-lg bg-light-200 justify-center items-center">
+
         <div className="w-full flex justify-end p-vmd items-center border-b-[1px] dark:border-b-light-500/30 border-b-light-500">
-          <h3 className='text-xl font-semibold lg:text-2xl dark:text-light-200 text-dark-200 sm:px-vmd px-vsm'>{title}</h3>
-          <button onClick={onClose} type="button" className="bg-transparent text-accent-dark-200 hover:text-accent-dark-100 hover:scale-110 active:scale-90 rounded-lg text-sm p-vsm ml-auto inline-flex items-center">
+          <div className="flex flex-wrap justify-start items-center sm:px-vmd px-vsm gap-x-vsm divide-x-[1px] divide-aimbotsRed-100">
+            <h3 className='text-xl font-semibold lg:text-2xl dark:text-light-200 text-dark-200'>{title}</h3>
+            <p className="px-vsm">{description}</p>
+          </div>
+          <button onClick={onClose} type="button" className="absolute top-0 right-0 bg-transparent text-accent-dark-200 hover:text-accent-dark-100 hover:scale-110 active:scale-90 rounded-lg text-sm p-vmd ml-auto inline-flex items-center">
             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>  
           </button>
         </div>
@@ -44,7 +49,7 @@ export const ProgramModalEntry = ({onClose}) => {
 
   if(slug === 'generator'){
     return (
-      <ProgramModalWrapper title="Program: Generator" onClose={onClose}>
+      <ProgramModalWrapper title="Program: Generator" description="The VIDYA Staking platform rewards the best performing stakers with a portion of the total VIDYA supply. The rewards are calculated based on the total amount of VIDYA staked in the network." onClose={onClose}>
         <GeneratorDapp/>
       </ProgramModalWrapper>
     )
