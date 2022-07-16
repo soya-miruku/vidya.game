41f7f70e41ad2265bd2c215918edce29f552c769
VinHylme <Lizodesignergfx@gmail.com>
2022-07-16 17:20:41 +0100

    videos will pause when modals are opened via context (ik i could use event listeners...) ....

--- components/atoms/Backdrop.tsx
+++ components/atoms/Backdrop.tsx
@@ -13,7 +13,7 @@ export const Backdrop = ({ children, onClick }: IBackdropProps) => {
   return (
     <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} style={{zIndex: 9999}} className='fixed top-0 left-0 h-full w-full bg-black/70 backdrop-blur-md flex justify-center items-center' 
       onClick={() => {
-        setShouldPause(() => false);
+        setShouldPause(false);
         onClick && onClick();
       }}>
       {children}
