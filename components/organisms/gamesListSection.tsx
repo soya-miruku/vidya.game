a9d0e0d82df97aa0d2676262f2e3a617b833b4ea
VinHylme <Lizodesignergfx@gmail.com>
2022-07-19 09:41:50 +0100

    build error bug fix

--- components/organisms/gamesListSection.tsx
+++ components/organisms/gamesListSection.tsx
@@ -18,7 +18,7 @@ export const GamesListSection: React.FC<IFetchPropPages> = ({limit=3}) => {
     <div className='flex gap-vlrg flex-wrap justify-center items-center py-vxl'>
       {(data || []).map((post, index) => {
         return (
-          <GameCard key={`${post.slug}_${index}`} label={post.customValues?.label} url={getPageUrlByType(post.type, post.slug)} image={post.meta?.featuredImage} title={post.meta.title} subtitle={post.meta.description} />
+          <GameCard key={`${post.slug}_${index}`} label={post.customValues?.label} url={getPageUrlByType(post.type, post.slug)} image={post.meta?.image?.src} title={post.meta.title} subtitle={post.meta.description} />
         )
       })}
     </div>
